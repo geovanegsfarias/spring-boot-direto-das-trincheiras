2217676a82d4a826998cf531c80c4a3fef91f7b0
Geovane <geovanegsf02@gmail.com>
2026-04-20 16:00:25 -0300

    video04: added hello rest controller and application.yml

---
+++ first-service/src/main/java/academy/devdojo/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package academy.devdojo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/greetings")
+public class HelloController {
+
+    @GetMapping("/hi")
+    public String hi() {
+        return "Hello World!";
+    }
+
+}
