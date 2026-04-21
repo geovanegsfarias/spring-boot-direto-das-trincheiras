81d44bad2bc84a9850fd471645c95858745e95db
Geovane <geovanegsf02@gmail.com>
2026-04-20 23:29:09 -0300

    video10: added findbyname hero with path variable

--- first-service/src/main/java/academy/devdojo/controllers/HeroController.java
+++ first-service/src/main/java/academy/devdojo/controllers/HeroController.java
@@ -1,9 +1,6 @@
 package academy.devdojo.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,10 @@ public class HeroController {
     @GetMapping("/filterList")
     public List<String> listAllHeroesParamList(@RequestParam List<String> names) {
         return HEROES.stream().filter(hero -> names.contains(hero)).toList();
+    }
+
+    @GetMapping("/{name}")
+    public String findByName(@PathVariable String name) {
+        return HEROES.stream().filter(hero -> hero.equalsIgnoreCase(name)).findFirst().orElse("");
     }
 }
