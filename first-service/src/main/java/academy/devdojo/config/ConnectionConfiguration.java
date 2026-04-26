ea1a6c6da91d3a25730d4ee941c506b71601bdd0
Geovane <geovanegsf02@gmail.com>
2026-04-26 19:34:49 -0300

    video49: using spring profiles and @value with properties value

---
+++ first-service/src/main/java/academy/devdojo/config/ConnectionConfiguration.java
@@ -0,0 +1,26 @@
+package academy.devdojo.config;
+
+import academy.devdojo.external.dependency.Connection;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConnectionConfiguration {
+    @Value("${database.url}")
+    private String url;
+    @Value("${database.username}")
+    private String username;
+    @Value("${database.password}")
+    private String password;
+
+    @Bean
+    public Connection connectionMySql() {
+        return new Connection(url, username, password);
+    }
+
+    @Bean(name = "connectionMongoDB")
+    public Connection connectionMongo() {
+        return new Connection("localhost", "devdojoMongo", "devdojo");
+    }
+}
---
+++ first-service/src/main/java/academy/devdojo/external/dependency/Connection.java
@@ -0,0 +1,14 @@
+package academy.devdojo.external.dependency;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@Getter
+public class Connection {
+    private String host;
+    private String username;
+    private String password;
+}
