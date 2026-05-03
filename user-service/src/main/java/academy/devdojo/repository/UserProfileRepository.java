399c531f7737499931eff5b27463d4f64042bf14
Geovane <geovanegsf02@gmail.com>
2026-05-03 15:57:25 -0300

    video97: logging query params and queries with JPQL

--- user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
+++ user-service/src/main/java/academy/devdojo/repository/UserProfileRepository.java
@@ -2,7 +2,15 @@ package academy.devdojo.repository;
 
 import academy.devdojo.domain.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
+    @Query("SELECT up FROM UserProfile up join fetch up.user u join fetch up.profile p")
+    List<UserProfile> retrieveAll();
 }
+
+// esse retrieveAll faz queries com JPQL (aula 97)
+// usando jpql, podemos otimizar as queries que usamos no projeto, o findAll() faz a mesma coisa que o retrieveAll, porém usa 5 queries para isso.
