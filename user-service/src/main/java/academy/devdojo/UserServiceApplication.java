c7e09585a9d5b22cf835eb9080d26601f259cb4c
Geovane <geovanegsf02@gmail.com>
2026-05-15 14:15:09 -0300

    video156: using @ConfigurationProperties

--- user-service/src/main/java/academy/devdojo/UserServiceApplication.java
+++ user-service/src/main/java/academy/devdojo/UserServiceApplication.java
@@ -1,9 +1,12 @@
 package academy.devdojo;
 
+import academy.devdojo.config.BrasilApiConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(BrasilApiConfigurationProperties.class)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
---
+++ user-service/src/main/java/academy/devdojo/controllers/BrasilApiController.java
@@ -0,0 +1,28 @@
+package academy.devdojo.controllers;
+
+import academy.devdojo.config.BrasilApiConfigurationProperties;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/brasil-api/cep")
+@Log4j2
+@RequiredArgsConstructor
+@SecurityRequirement(name = "basicAuth")
+public class BrasilApiController {
+
+    private final BrasilApiConfigurationProperties brasilApiConfigurationProperties;
+
+    @GetMapping
+    public ResponseEntity<Void> findCep() {
+        log.info("Request received to find cep");
+        log.info("base-url {}, cepUri {}", brasilApiConfigurationProperties.baseUrl(), brasilApiConfigurationProperties.cepUri());
+        return ResponseEntity.noContent().build();
+    }
+}
